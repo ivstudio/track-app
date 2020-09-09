5b0844435b63c767705f0f9ecf26ee366eb22da6
Iggy Villamar <iggy@ivstudio.com>
2020-09-08 21:10:27 -0400

    Add spacing scale and color variables

---
+++ src/styles/colors.ts
@@ -0,0 +1,12 @@
+const colors = {
+	white: '#ffffff',
+	red: '#b60205',
+	green: '#28a745',
+	gray10: '#f6f8fa',
+	gray20: '#e1e4e8',
+	gray30: '#586069',
+	blue30: '#0366d6',
+	blue90: '#24292e',
+};
+
+export default colors;
