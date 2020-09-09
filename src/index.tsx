22fe8a253001767e9c679adf1f540163e16755fb
Iggy Villamar <iggy@ivstudio.com>
2020-09-08 20:34:39 -0400

    Add styled-normalize and import Normalize to the root

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { Normalize } from 'styled-normalize';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
 	<React.StrictMode>
+		<Normalize />
 		<App />
 	</React.StrictMode>,
 	document.getElementById('root')
