37b80e5abdd9cbcc1ea9d28efe534e848167fbfe
Iggy Villamar <iggy@ivstudio.com>
2020-09-12 19:24:00 -0400

    Updates

---
+++ precache-manifest.e95ff49592b00dadd80cabd5d64e524a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42f7c1468fbe6b56e82be10478586b5b",
+    "url": "/track-app/index.html"
+  },
+  {
+    "revision": "3e105fb28d199f1a371e",
+    "url": "/track-app/static/js/2.52b4b669.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/track-app/static/js/2.52b4b669.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb6ea979c26e07929f85",
+    "url": "/track-app/static/js/main.f39c475c.chunk.js"
+  },
+  {
+    "revision": "67421a685136e2c43aae",
+    "url": "/track-app/static/js/runtime-main.34d6064f.js"
+  }
+]);
