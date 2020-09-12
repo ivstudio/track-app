f6e0b7edd27810db3d0b9bfb9edc97afa0868497
Iggy Villamar <iggy@ivstudio.com>
2020-09-12 17:58:57 -0400

    Remove api service

--- src/utils/api.service.ts
+++
@@ -1,7 +0,0 @@
-export async function fetchData<T>(request: RequestInfo): Promise<T> {
-	const res = await fetch(request);
-	if (!res.ok) {
-		throw new Error(res.statusText);
-	}
-	return res.json();
-}
