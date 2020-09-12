f4aab27627ae7754298b6c3af7824d12c321d908
Iggy Villamar <iggy@ivstudio.com>
2020-09-12 17:45:17 -0400

    Add axios mock function and mock data for testing

---
+++ src/__test__/issues.data.ts
@@ -0,0 +1,30 @@
+export const issues = [
+	{
+		title: 'Feature request: bind event to react hook component.',
+		number: 19823,
+		url: 'https://api.github.com/repos/facebook/react',
+		labels: [
+			{
+				id: 700183312,
+				color: 'd4c5f9',
+				name: 'Status: Unconfirmed',
+			},
+		],
+		created_at: '2020-09-12T10:29:24Z',
+		user: { login: 'thinksource' },
+	},
+	{
+		title: 'Enable source maps for DevTools production builds',
+		number: 19773,
+		url: 'https://api.github.com/repos/facebook/react',
+		labels: [
+			{
+				id: 196858374,
+				color: 'e7e7e7',
+				name: 'CLA Signed',
+			},
+		],
+		created_at: '2020-09-07T18:28:14Z',
+		user: { login: 'jpribyl' },
+	},
+];
