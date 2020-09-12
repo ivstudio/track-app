f308827fe219867b106c5186d906d4669d7e33a5
Iggy Villamar <iggy@ivstudio.com>
2020-09-12 17:59:26 -0400

    Delete default create-react-app test

--- src/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
-
-test('renders initial', () => {
-	const { getByText } = render(<App />);
-	const linkElement = getByText(/initial/i);
-	expect(linkElement).toBeInTheDocument();
-});
